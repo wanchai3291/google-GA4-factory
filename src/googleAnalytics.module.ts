7ad4acee3ec47f91ef08481caac624b53a600c6d
Wanchai Sirimat <wanchai.s@epic-consulting.net>
2023-06-26 14:05:04 +0700

    no message

--- src/googleAnalytics.module.ts
+++ src/googleAnalytics.module.ts
@@ -1,16 +1,16 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { GoogleAnalyticsOptions } from './options';
-import { GoogleAnalyticsService } from './googleAnalytics.service';
+import { DynamicModule, Module } from "@nestjs/common";
+import { GoogleAnalyticsOptions } from "./options";
+import { GoogleAnalyticsService } from "./googleAnalytics.service";
 
 @Module({})
 export class GoogleAnalyticsModule {
-    static forRoot(
-        options: GoogleAnalyticsOptions,
-    ): DynamicModule {
-        return {
-            providers: [],
-            exports: [GoogleAnalyticsService],
-            module: GoogleAnalyticsModule,
-        };
-    }
+  static forRoot(options: GoogleAnalyticsOptions): DynamicModule {
+    return {
+      global: true,
+      module: GoogleAnalyticsModule,
+      imports: [],
+      providers: [],
+      exports: [],
+    };
+  }
 }

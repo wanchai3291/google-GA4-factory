f7c8b1c1483b1783702a80b6f5ff25ddef3d31de
Wanchai Sirimat <wanchai.s@epic-consulting.net>
2023-06-26 15:37:58 +0700

    no message

--- src/ga4/ga4.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { Ga4Service } from './ga4.service';
-import { HttpModule } from '@nestjs/axios';
-
-@Module({
-  imports: [HttpModule],
-  providers: [Ga4Service]
-})
-export class Ga4Module {}
--- src/googleAnalytics.module.ts
+++ src/googleAnalytics.module.ts
@@ -1,15 +1,15 @@
 import { DynamicModule, Module } from "@nestjs/common";
-import { GoogleAnalyticsOptions } from "./options";
-import { GoogleAnalyticsService } from "./googleAnalytics.service";
+// import { GoogleAnalyticsOptions } from "./options";
+// import { GoogleAnalyticsService } from "./googleAnalytics.service";
 import { HttpModule, HttpService } from "@nestjs/axios";
-import { Ga4Module } from './ga4/ga4.module';
+
 
 @Module({})
 export class GoogleAnalyticsModule {
   static forRoot(options: {}): DynamicModule {
     return {
       module: GoogleAnalyticsModule,
-      imports:[HttpModule.register({}),Ga4Module],
+      imports:[HttpModule],
       providers: [HttpService],
       exports: [HttpService],
     };

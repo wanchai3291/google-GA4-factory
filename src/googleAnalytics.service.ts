8f3a0d69df4c1e238b81dce97cf81aeb694ce444
Wanchai Sirimat <wanchai.s@epic-consulting.net>
2023-06-26 15:24:08 +0700

    no message

--- src/googleAnalytics.service.ts
+++ src/googleAnalytics.service.ts
@@ -6,21 +6,21 @@ import { firstValueFrom } from 'rxjs';
 @Injectable()
 export class GoogleAnalyticsService {
 
-    // constructor(private readonly httpService: HttpService) {}
+    constructor(private readonly httpService: HttpService) {}
 
     async generate() {
         const url = 'https://jsonplaceholder.typicode.com/todos/1';
-        // const requestConfig: AxiosRequestConfig = {
-        //   method: 'get',
-        //   url: url,
-        //   headers: {
-        //     'Content-Type': 'application/json'
-        //   },
-        //   params: {},
-        // }
-        // const result = await firstValueFrom(
-        //   this.httpService.request(requestConfig),
-        // );
-        // return result;
+        const requestConfig: AxiosRequestConfig = {
+          method: 'get',
+          url: url,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          params: {},
+        }
+        const result = await firstValueFrom(
+          this.httpService.request(requestConfig),
+        );
+        return result;
     }
 }
